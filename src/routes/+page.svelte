3fb9cca0e2d2a93388ae7e3d30b9cbac90d08753
Jacqueline Guo <guojacqu@usc.edu>
2026-06-06 22:50:37 +0800

    add bare bones html

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,43 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import { resolve } from '$app/paths';
+</script>
+
+<div>
+    <h1>Jacqueline Guo</h1>
+    <p>Hey! This is Jacqueline-</p>
+    <p>Product & graphic designer, amateur engineer, and recent USC CS Games grad (fight on!) based in the San Francisco Bay Area.</p>
+    <p>I’m excited by solving hard problems with great people to make our world a better place, so I’m stoked to be starting at Persona soon getting to do just that.</p>
+    <p>Outside of work, I’m probably nerding out over typography, photographing everyday life, trying a new handcraft, or being a lifelong music student. Right now, I’m also learning more about frontend and building on the web. You can read my design engineering field notes here, amongst other writing.</p>
+    <p>If any of this speaks to you, please drop me a line. I’d love to chat!</p>
+    <nav>
+        <a href={resolve('/work')}>Work</a>
+        <a href={resolve('/sandbox')}>Sandbox</a>
+        <a href={resolve('/writing')}>Writing</a>
+        <a href={resolve('/about')}>About</a>
+    </nav>
+</div>
+
+<div>
+    <h2>Selected Work</h2>
+    <img src={resolve('/favicon.svg')} alt="temp favicon">
+
+    <h3>Rabbitholing</h3>
+    <p>Rabbithole into topics using LLMs by transforming chat responses into a knowledge graph nodes and links.</p>
+</div>
+
+<div>
+    <h2>Notes from the Garden</h2>
+    <ol>
+        <li>Design Engineering Fieldnotes</li>
+        <li>Graduating from USC? A checklist, so you can enjoy your time left</li>
+        <li>Don’t delegate understanding</li>
+    </ol>
+</div>
+
+<div>
+    <img src={resolve('/favicon.svg')} alt="temp place for photography">
+</div>
+
+<div>
+    <p>random div that idk what to do with rn</p>
+</div>
