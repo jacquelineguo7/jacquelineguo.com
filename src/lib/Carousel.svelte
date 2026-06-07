2941f4b6c3d5107f8b0220a4567e1ca0d4f730c2
Jacqueline Guo <guojacqu@usc.edu>
2026-06-08 00:49:00 +0800

    start carousel svelte component

---
+++ src/lib/Carousel.svelte
@@ -0,0 +1,17 @@
+<!-- Carousel.svelte -->
+
+<!-- JS -->
+<!-- Components are reusable. Funnel unique data in using properties (props for short.) -->
+<!-- Aka function parameters. Someone calls the component "function" and the relevant params/args are passed. -->
+
+<script>
+    let { projects } = $props(); // Props is a Svelte function that returns all params/args passed in as an object.
+</script>
+
+<!-- HTML MARKUP -->
+
+
+<!-- CSS STYLES -->
+<style>
+
+</style>
