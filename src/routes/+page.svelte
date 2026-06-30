c12a024aa870fd31f781dccd75cad6bb372bd999
Jacqueline Guo <guojacqu@usc.edu>
2026-06-29 18:46:08 -0700

    added envelope underneath postcard

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -118,7 +118,7 @@
                             <p class="top-p">Hey! This is Jacqueline.</p>
                             <p>I'm a product + graphic designer, amateur engineer, and recent <a href="https://games.usc.edu/" class="inline-link">USC CS Games</a> grad (fight on!) based in the San Francisco Bay Area.</p>
                             <p>I’m excited by tackling hard, technical problems with great people to make our world a better place, and so I’m stoked to be starting at <a href="https://withpersona.com/" class="inline-link">Persona</a> soon. Previously, I was a design intern at <a href="https://developer.apple.com/xcode/" class="inline-link">Apple</a> working on dev tooling and AI.</p>
-                            <p>Outside of work, I’m probably nerding out over typography, photographing everyday life, trying a new handcraft, or being a lifelong music student. Right now, I’m learning the technical details behind the web. You can read my field notes <a href={resolve('/writing')} class="inline-link">here</a>, amongst other writing.</p>
+                            <p>Outside of work, I’m probably nerding out over typography, photographing everyday life, trying a new handcraft, or being a lifelong music student. Right now, I’m learning about how the web was built and how that contributes to its current state. You can read my field notes <a href={resolve('/writing')} class="inline-link">here</a>, amongst other writing.</p>
                             <p class="bottom-p">If any of this speaks to you, please <a href="mailto:jacquelineguo7@gmail.com?subject=something in your intro caught my eye" class="inline-link">drop me a line</a>. I’d love to chat!</p>
                         </div>
                         <nav id="nav">
@@ -145,59 +145,61 @@
                 </div>
             </div>
             <div class="col" id="right-col">
-                <div id="selected-work" bind:this={stage}>
-                    <div class="envelope-decor">
-                        <div class="env-left">
-                            <h2 id="feature">Featured Work</h2>
-                            <div class="line-media-container"></div>
+                <div id="envelope">
+                    <div id="envelope-flap"></div>
+                    <div id="selected-work" bind:this={stage}>
+                        <div class="postcard-decor">
+                            <div class="pc-left">
+                                <h2 id="feature">Featured Work</h2>
+                                <div class="line-media-container"></div>
+                            </div>
+                            <div class="pc-right">
+                                <div class="stamp-div" class:dragging class:over={overZone} bind:this={dropZone}>
+                                    <!-- bind:this hands you that node after render -->
+                                    <p class="stamp-text">Place<br>Stamp<br>Here</p>
+                                </div>
+                            </div>
                         </div>
-                        <div class="env-right">
-                            <div class="stamp-div" class:dragging class:over={overZone} bind:this={dropZone}>
-                                <!-- bind:this hands you that node after render -->
-                                <p class="stamp-text">Place<br>Stamp<br>Here</p>
+
+                        <div
+                            class="floating-item"
+                            style:--card-w="{cardbingo.width}rem"
+                            style:transform="translate3d({cardbingo.x}px, {cardbingo.y}px, 0) rotate({cardbingo.rotation}deg)"
+                            style:z-index={cardbingo.z}
+                            use:draggable={cardbingo}
+                        >
+                            <div class="example-card">
+                                <img src="/projects/bingo/stamp.png" alt="xcode" draggable="false">
+                                <!-- <span class="coord-readout">{Math.round(cardbingo.x)}, {Math.round(cardbingo.y)}</span> -->
+                            </div>
+                        </div>
+
+                        <div
+                            class="floating-item"
+                            style:--card-w="{cardxcode.width}rem"
+                            style:transform="translate3d({cardxcode.x}px, {cardxcode.y}px, 0) rotate({cardxcode.rotation}deg)"
+                            style:z-index={cardxcode.z}
+                            use:draggable={cardxcode}
+                        >
+                            <div class="example-card">
+                                <img src="/projects/xcode/stamp.png" alt="xcode" draggable="false">
+                                <!-- <span class="coord-readout">{Math.round(cardxcode.x)}, {Math.round(cardxcode.y)}</span> -->
+                            </div>
+                        </div>
+
+                        <div
+                            class="floating-item"
+                            style:--card-w="{cardrabbit.width}rem"
+                            style:transform="translate3d({cardrabbit.x}px, {cardrabbit.y}px, 0) rotate({cardrabbit.rotation}deg)"
+                            style:z-index={cardrabbit.z}
+                            use:draggable={cardrabbit}
+                        >
+                            <div class="example-card">
+                                <img src="/projects/rabbitholing/stamp.png" alt="xcode" draggable="false">
+                                <!-- <span class="coord-readout">{Math.round(cardrabbit.x)}, {Math.round(cardrabbit.y)}</span> -->
                             </div>
                         </div>
                     </div>
-
-                    <div
-                        class="floating-item"
-                        style:--card-w="{cardbingo.width}rem"
-                        style:transform="translate3d({cardbingo.x}px, {cardbingo.y}px, 0) rotate({cardbingo.rotation}deg)"
-                        style:z-index={cardbingo.z}
-                        use:draggable={cardbingo}
-                    >
-                        <div class="example-card">
-                            <img src="/projects/bingo/stamp.png" alt="xcode" draggable="false">
-                            <!-- <span class="coord-readout">{Math.round(cardbingo.x)}, {Math.round(cardbingo.y)}</span> -->
-                        </div>
-                    </div>
-
-                    <div
-                        class="floating-item"
-                        style:--card-w="{cardxcode.width}rem"
-                        style:transform="translate3d({cardxcode.x}px, {cardxcode.y}px, 0) rotate({cardxcode.rotation}deg)"
-                        style:z-index={cardxcode.z}
-                        use:draggable={cardxcode}
-                    >
-                        <div class="example-card">
-                            <img src="/projects/xcode/stamp.png" alt="xcode" draggable="false">
-                            <!-- <span class="coord-readout">{Math.round(cardxcode.x)}, {Math.round(cardxcode.y)}</span> -->
-                        </div>
-                    </div>
-
-                    <div
-                        class="floating-item"
-                        style:--card-w="{cardrabbit.width}rem"
-                        style:transform="translate3d({cardrabbit.x}px, {cardrabbit.y}px, 0) rotate({cardrabbit.rotation}deg)"
-                        style:z-index={cardrabbit.z}
-                        use:draggable={cardrabbit}
-                    >
-                        <div class="example-card">
-                            <img src="/projects/rabbitholing/stamp.png" alt="xcode" draggable="false">
-                            <!-- <span class="coord-readout">{Math.round(cardrabbit.x)}, {Math.round(cardrabbit.y)}</span> -->
-                        </div>
-                    </div>
-
                 </div>
 
                 <div id="other">
@@ -426,6 +428,24 @@
         
     }
 
+    #envelope {
+        background-color: #D7DDE2;
+        height: 60vh;          /* gives the flap's % height something to resolve against */
+        position: relative;
+        padding: 3rem;
+    }
+
+    #envelope-flap {
+        position: absolute;
+        top: 0;                    
+        left: 0;
+        right: 0;
+        height: 35%;
+        background-color: #C5CDD4;
+        border-bottom-left-radius: 1rem;
+        border-bottom-right-radius: 1rem;
+    }
+
     #selected-work {
         position: relative;   /* the stage: floating items measure x/y from here */
         min-height: 45vh;    /* canvas room — absolute items don't prop this open */
@@ -436,20 +456,20 @@
         gap: 2rem;
     }
 
-    .envelope-decor {
+    .postcard-decor {
         flex: 1;
         display: flex;
         flex-direction: row;
         justify-content: space-between;
     }
 
-    .env-left {
+    .pc-left {
         width: 12rem;
         display: flex;
         flex-direction: column;
     }
 
-    .env-right {
+    .pc-right {
         display: flex;
         justify-content: end;
     }
