97395205e04cc292bea0ee5df4c92172175ca9b3
Jacqueline Guo <guojacqu@usc.edu>
2026-07-01 13:28:54 -0700

    remove dialkit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,5 @@
 <script>
 	import favicon from '$lib/assets/slanted-saturated-button.svg';
-	// import { createDialKit } from 'dialkit/svelte'
-	import 'dialkit/styles.css'
 
 	let { children } = $props();
 	import '../app.css';
